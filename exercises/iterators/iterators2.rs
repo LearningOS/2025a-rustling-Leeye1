d5acca8de40ba2a39cc89b277e11aab9ec40ce44
Yi Ling <ly1379391339@gmail.com>
2025-09-16 17:23:48 +0800

    Update

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -23,7 +23,9 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
+    //这里是因为iter产生迭代器的item是&&str类型，所以要制定input是&&str
     words.iter().map(|input: &&str| capitalize_first(*input)).collect::<Vec<String>>()
+
 }
 
 // Step 3.
@@ -31,6 +33,7 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
+    //这里传入了capitalize_first作为Map<Self,F>的函数，每次迭代产生的Item都会调用f来产生新的元素
     words.iter().map(|input: &&str| capitalize_first(*input)).collect::<String>()
 }
 
