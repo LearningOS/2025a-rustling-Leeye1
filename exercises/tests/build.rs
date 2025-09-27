b53673b9b991c601cf96820402b99a77dae56909
Yi Ling <ly1379391339@gmail.com>
2025-09-27 12:15:06 +0800

    update

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -6,19 +6,16 @@ fn main() {
     // In tests7, we should set up an environment variable
     // called `TEST_FOO`. Print in the standard output to let
     // Cargo do it.
-    let timestamp = std::time::SystemTime::now()
-        .duration_since(std::time::UNIX_EPOCH)
+    let timestamp = std::time::SystemTime::now()//当前系统时间
+        .duration_since(std::time::UNIX_EPOCH)//1970-01-01 0点到现在过去了多少秒
         .unwrap()
-        .as_secs(); // What's the use of this timestamp here?
-    let your_command = format!(
-        "Your command here with {}, please checkout exercises/tests/build.rs",
-        timestamp
-    );
-    println!("cargo:{}", your_command);
+        .as_secs(); // What's the use of this timestamp here? //转换成整数（s）
+    println!("cargo:rustc-env=TEST_FOO={}", timestamp);
+    
 
     // In tests8, we should enable "pass" feature to make the
     // testcase return early. Fill in the command to tell
     // Cargo about that.
-    let your_command = "Your command here, please checkout exercises/tests/build.rs";
+    let your_command = format!("rustc-cfg=feature=\"pass\"");;
     println!("cargo:{}", your_command);
 }
--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,15 +27,16 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-// I AM NOT DONE
 
-extern "Rust" {
+extern "Rust" {//这里告诉编译器我要使用一个外部的函数
     fn my_demo_function(a: u32) -> u32;
+    #[link_name="my_demo_function"]
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
 mod Foo {
     // No `extern` equals `extern "Rust"`.
+    #[no_mangle] //这里告诉rust编译器不需要修饰函数名符号
     fn my_demo_function(a: u32) -> u32 {
         a
     }
